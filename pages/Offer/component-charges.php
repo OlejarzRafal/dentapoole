cc7e660a9efe9b4e986998cef571c0e3072df4e3
Rafał Olejarz <olejarz.rafal89@gmail.com>
2026-04-21 19:58:13 +0200

    fi

--- pages/Offer/component-charges.php
+++ pages/Offer/component-charges.php
@@ -1,26 +1,33 @@
-<section data-id="charges" id="nhs-price-list" class="py-[120px] bg-light max-lg:pt-[80px] max-lg:pb-[60px]">
-  <div class="charges__inner">
-    <div class="charges__container">
-      <div class="charges__text" data-opacity-move><?php the_field('charges-text'); ?></div>
-      <div class="grid grid-cols-3 gap-[21px]  max-xl:ml-0  max-lg:mx-auto max-lg:grid-cols-1 max-lg:gap-[60px]">
-        <?php if (have_rows('charges-list')): ?>
-          <?php while (have_rows('charges-list')):
-            the_row();
-            $image = get_sub_field('image');
-            $title = get_sub_field('title');
-            $price = get_sub_field('price');
-            $text = get_sub_field('text');
-            ?>
-            <div class="charges__item">
-              <div class="charges__image" data-opacity-move>
-                <img src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>" />
+<?php 
+$charges_text = get_field('charges-text');
+// Usuń wszystkie tagi HTML i sprawdź czy coś zostało
+$text_content = strip_tags($charges_text);
+if (!empty($text_content) && trim($text_content) !== ''): 
+?>
+  <section data-id="charges" id="nhs-price-list" class="py-[120px] bg-light max-lg:pt-[80px] max-lg:pb-[60px]">
+    <div class="charges__inner">
+      <div class="charges__container">
+        <div class="charges__text" data-opacity-move><?php echo $charges_text; ?></div>
+        <div class="grid grid-cols-3 gap-[21px]  max-xl:ml-0  max-lg:mx-auto max-lg:grid-cols-1 max-lg:gap-[60px]">
+          <?php if (have_rows('charges-list')): ?>
+            <?php while (have_rows('charges-list')):
+              the_row();
+              $image = get_sub_field('image');
+              $title = get_sub_field('title');
+              $price = get_sub_field('price');
+              $text = get_sub_field('text');
+              ?>
+              <div class="charges__item">
+                <div class="charges__image" data-opacity-move>
+                  <img src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>" />
+                </div>
+                <div class="charges__title" data-opacity-move><?= $title; ?> <span><?= $price; ?></span></div>
+                <div class="charges__desc" data-opacity-move><?= $text; ?></div>
               </div>
-              <div class="charges__title" data-opacity-move><?= $title; ?> <span><?= $price; ?></span></div>
-              <div class="charges__desc" data-opacity-move><?= $text; ?></div>
-            </div>
-          <?php endwhile; ?>
-        <?php endif; ?>
+            <?php endwhile; ?>
+          <?php endif; ?>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+<?php endif; ?>
--- pages/Offer/component-free.php
+++ pages/Offer/component-free.php
@@ -1,16 +1,22 @@
-<section data-id="free">
-  <div class="free__inner">
-    <div class="free__container">
-      <div class="free__main">
-        <div class="free__text" data-opacity-move><?php the_field('free-text'); ?></div>
-        <div class="free__image" data-opacity-move>
-          <?php
-          $image = get_field('free-image');
-          if (!empty($image)): ?>
-            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-          <?php endif; ?>
+<?php 
+$free_text = get_field('free-text');
+$text_content = strip_tags($free_text);
+if (!empty($text_content) && trim($text_content) !== ''): 
+?>
+  <section data-id="free">
+    <div class="free__inner">
+      <div class="free__container">
+        <div class="free__main">
+          <div class="free__text" data-opacity-move><?php echo $free_text; ?></div>
+          <div class="free__image" data-opacity-move>
+            <?php
+            $image = get_field('free-image');
+            if (!empty($image)): ?>
+              <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+            <?php endif; ?>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+<?php endif; ?>
